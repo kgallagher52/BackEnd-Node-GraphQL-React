50cf74ff8deca6f4f100e2fe21fb9e5d2d917f57
Keaton Gallagher <d00249750@dmail.dixie.edu>
2019-04-25 01:51:41 -0600

    added the relationship from authors to books using list type

--- schemas/schema.js
+++ schemas/schema.js
@@ -7,7 +7,8 @@ const {
     GraphQLString, 
     GraphQLSchema,
     GraphQLID,
-    GraphQLInt
+    GraphQLInt,
+    GraphQLList
 } = graphql;
 
 //Dummie Data
@@ -15,14 +16,21 @@ var books = [
     {name:"The Great Panda", genre:"Fantasy",id:"1",authorId:"1"},
     {name:"No Sleep", genre:"Fantasy",id:"2",authorId:"2"},
     {name:"Dev Mountain", genre:"Fantasy",id:"3",authorId:"3"},
-    {name:"Great Tails", genre:"Biography",id:"4",authorId:"4"}
+    {name:"Great Tails", genre:"Biography",id:"4",authorId:"4"},
+    {name:"Magical Beans", genre:"Fantasy",id:"5",authorId:"5"},
+    {name:"Ever Lasting Life", genre:"Biography",id:"6",authorId:"6"},
+    {name:"My coding Life", genre:"Biography",id:"7",authorId:"1"}
+
 ];
 
 var authors = [
     {name:"Keaton", age:21,id:"1"},
     {name:"Siri", age:30,id:"2"},
     {name:"Cameron", age:31,id:"3"},
-    {name:"Tom", age:28,id:"4"}
+    {name:"Tom", age:28,id:"4"},
+    {name:"Alex", age:31,id:"5"},
+    {name:"Jeremy", age:25,id:"6"}
+
 ];
 
 const BookType = new GraphQLObjectType({ // Object Type
@@ -42,10 +50,16 @@ const BookType = new GraphQLObjectType({ // Object Type
 
 const AuthorType = new GraphQLObjectType({ // Object Type
     name: 'Author',
-    fields: () => ({
+    fields: () => ({ //Needs to be a function so that the code is executed when it's needed otherwise it would not know what the fields are.
         id: { type:GraphQLID },
         name: { type:GraphQLString },
-        age: { type:GraphQLInt }
+        age: { type:GraphQLInt },
+        books: {
+            type: new GraphQLList(BookType),
+            resolve(parent,args) {
+                return _.filter(books, {authorId: parent.id})
+            }
+        }
 
     })
 })
